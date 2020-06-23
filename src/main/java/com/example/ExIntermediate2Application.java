7acdb41a1daff363cd40ce10fd5a6bd813a20ada
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-23 16:10:29 +0900

    中級課題

---
+++ src/main/java/com/example/ExIntermediate2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExIntermediate2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExIntermediate2Application.class, args);
+	}
+
+}
